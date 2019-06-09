347d0a2d56c3385f9ffb37c16a5187fae02e80d7
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-09 17:38:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,10 @@
 def oxford_comma(array)
-if array.length > 4 
-  array.join(",")
+if 
   elsif array.length == 2 
   array.join(" and ")
+  elsif array.length == 3
+  array.join
+  elsif array.length > 4 
+  array.join(",")
 end 
 end
