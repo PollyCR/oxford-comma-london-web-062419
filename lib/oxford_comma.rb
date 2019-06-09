6a9ae10be1cd2c1c7a1562cbfb03798a473e8da1
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-09 17:39:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
-if 
+if array.length == 1 
+  array.join
   elsif array.length == 2 
   array.join(" and ")
   elsif array.length == 3
   array.join
-  elsif array.length > 4 
+  elsif array.length >= 4 
   array.join(",")
 end 
 end
